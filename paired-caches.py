50db774902d211c25b638befbb8328f1170a64db
Jeremy Rand <jeremyrand@danwin1210.de>
2023-05-05 04:35:46 +0000

    Fix cpuset parsing on 2x18-core Talos II systems

--- paired-caches.py
+++ paired-caches.py
@@ -19,7 +19,22 @@ for element in root.iter("*"):
         cpuset = element.get("cpuset")
         if cpuset is None:
             continue
-        caches.append(int(cpuset, 0))
+
+        # cpuset is an arbitrary-length integer, encoded as a big-endian
+        # comma-separated list of hex-encoded 32-bit integers. Each 32-bit
+        # integer has a leading "0x"; if the 32-bit integer is 0, it can be
+        # encoded as an empty string. However, we take a shortcut here, because
+        # on real-world Talos hardware, only the most significant 32-bit
+        # integer is non-zero, so we simply take the most significant 32-bit
+        # integer, and apply a bitshift based on the count of 32-bit integers.
+        cpuset = cpuset.split(',')
+        cpuset_shift = len(cpuset)
+        cpuset_shift = cpuset_shift - 1
+        cpuset_shift = cpuset_shift * 32
+        cpuset = int(cpuset[0], 0)
+        cpuset = cpuset << cpuset_shift
+
+        caches.append(cpuset)
 
 all_disable = set()
 
